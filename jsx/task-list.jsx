f587a573bd82e44dc677979c15c4b7caf8f3a7b7
Benji Shine <git@bshine.net>
2018-01-31 12:48:03 -0800

    added object literal and looped through it in task list

--- jsx/task-list.jsx
+++ jsx/task-list.jsx
@@ -1,5 +1,10 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+const thingy = [
+  { done:false, title:"Do stuff" },
+  { done:true, title:"Do moar stuff"},
+  { done:false, title:"blah"}
+];
 
 class TaskList extends React.Component {
   constructor(props) {
@@ -10,8 +15,12 @@ class TaskList extends React.Component {
       <div className="well">
         <h1>TaskList</h1>
         <ul> 
-            <li> myTask </li>
-            <li> myTask </li>
+          <li> "yay" {thingy[0].title}</li>
+            { 
+              thingy.map((task, index) => { 
+               return <li key={index}>  {task.title} </li>
+              })
+            }
         </ul>
       </div>
     )
