607342ec3630c5b1b0aa12f74d91275027d210c8
Benji Shine <git@bshine.net>
2018-02-04 14:43:47 -0800

    actually reneder todo app from tutorial

--- src/App.js
+++ src/App.js
@@ -262,21 +262,9 @@ const TodoApp = () => (
     <Footer />
   </div>
 );
+const WrappedApp = () =>
+    <Provider store={createStore(todoApp)}>
+  <TodoApp />
+</Provider>;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default WrappedApp;
